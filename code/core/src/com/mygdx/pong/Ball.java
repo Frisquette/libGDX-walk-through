8a4d4384cedd7df07684b6d795466e16a7a6c4f6
David Peicho <david.peicho@epita.fr>
2016-08-08 14:47:07 +0200

    Update racket and ball speed

--- code/core/src/com/mygdx/pong/Ball.java
+++ code/core/src/com/mygdx/pong/Ball.java
@@ -9,14 +9,13 @@ public class Ball extends Sprite {
     private final Vector2 normal = new Vector2(0, 1);
     private Vector2 direction_;
 
-    public Ball(int x, int y) {
+    public Ball() {
         super(new Texture("pong/ball.png"));
-
-        this.setPosition(x, y);
     }
 
-    public void update() {
-        this.translate(direction_.x * 8, direction_.y * 8);
+    public void update(float deltaTime) {
+        this.translate(direction_.x * Const.BALL_SPEED * deltaTime,
+                       direction_.y * Const.BALL_SPEED * deltaTime);
     }
 
     public void setDirection(Vector2 direction) {
@@ -26,5 +25,13 @@ public class Ball extends Sprite {
 
     public Vector2 getDirection() {
         return direction_;
+    }
+
+    public void init(int x, int y) {
+        this.setPosition(x, y);
+        if (2 * (float)Math.random() - 1.0f <= 0.0f)
+            setDirection(new Vector2(-1.0f, 0.0f));
+        else
+            setDirection(new Vector2(1.0f, 0.0f));
     }
 }
--- code/core/src/com/mygdx/pong/Racket.java
+++ code/core/src/com/mygdx/pong/Racket.java
@@ -5,19 +5,28 @@ import com.badlogic.gdx.graphics.g2d.Sprite;
 
 public class Racket extends Sprite {
 
+    int initX_;
+    int initY_;
+
     public Racket(int x, int y) {
         super(new Texture("pong/racket.png"));
 
-        this.setPosition(x, y);
+        initX_ = x;
+        initY_ = y;
+        this.setPosition(x, y  - this.getHeight() / 2);
     }
 
-    public void moveUp() {
+    public void moveUp(float deltaTime) {
         if (this.getY() + this.getHeight() < 580)
-            this.translateY(7.0f);
+            this.translateY(Const.RACKET_SPEED * deltaTime);
     }
 
-    public void moveDown() {
+    public void moveDown(float deltaTime) {
         if (this.getY() > 20)
-            this.translateY(-7.0f);
+            this.translateY(- Const.RACKET_SPEED * deltaTime);
+    }
+
+    public void init() {
+        this.setPosition(initX_, initY_ - this.getHeight() / 2);
     }
 }
